78c80d8cae7b14650110938e200e4fe16a735887
shlommi <shlommi@gmail.com>
2022-05-21 18:59:29 +0300

    finished project

---
+++ src/components/Quiz.js
@@ -0,0 +1,45 @@
+import React, { useContext } from 'react';
+import { QuizContext } from '../contexts/quiz';
+import Question from './Question';
+const Quiz = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+  console.log('quizState', quizState);
+  return (
+    <div className='quiz'>
+      {quizState.showResults && (
+        <div className='results'>
+          <div className='congratulations'>Congratulations</div>
+          <div className='results-info'>you have completed the quiz</div>
+          <div>
+            Yoy have got {quizState.correctAnswersCount} of{' '}
+            {quizState.questions.length} right
+          </div>
+          <div
+            className='next-button'
+            onClick={() => dispatch({ type: 'RESTART' })}
+          >
+            {' '}
+            Restart
+          </div>
+        </div>
+      )}
+      {!quizState.showResults && (
+        <>
+          <div className='score'>
+            Question {quizState.currentQuestionIndex + 1} /{' '}
+            {quizState.questions.length}
+          </div>
+          <Question />
+          <div
+            className='next-button'
+            onClick={() => dispatch({ type: 'NEXT_QUESTION' })}
+          >
+            Next Question
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Quiz;
